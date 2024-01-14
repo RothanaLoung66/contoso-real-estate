bbc15e6e018cf0ca1ff68661cba5dfd420f4c300
Rothana Loung <151432205+RothanaLoung66@users.noreply.github.com>
2024-01-14 20:35:25 +0700

    update firebase
    Signed-off-by: Rothana Loung <151432205+RothanaLoung66@users.noreply.github.com>

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-$ npm install -g firebase-tools
-$ firebase login
-$ firebase init
+npm install -g
+firebase-tools
+firebase login
+firebase init
